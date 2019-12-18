c5f621262ad73c9bd2b72f81f19332d77a81f949
sharif <sharifmostafa1122@gmail.com>
2019-12-19 02:04:26 +0300

    connect img fields in imgs and modifies listings

--- listings/admin.py
+++ listings/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import listing
+from .models import Listing
 
 class ListingAdmin(admin.ModelAdmin):
     list_display=('id','title','is_published','list_date','realtor')
@@ -9,4 +9,4 @@ class ListingAdmin(admin.ModelAdmin):
     list_editable=('is_published',)
     search_fields=('title','description','address','city','state','zipcode','price')
     list_per_page=25
-admin.site.register(listing,ListingAdmin)
+admin.site.register(Listing,ListingAdmin)
