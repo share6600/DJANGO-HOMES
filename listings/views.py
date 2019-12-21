8986d0e6705e5aecb641e7791d62d2f644653aa9
sharif <sharifmostafa1122@gmail.com>
2019-12-21 19:06:23 +0300

    import pagination and prepare paginator method to handel listings requests from database

--- listings/views.py
+++ listings/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage,Paginator,PageNotAnInteger
 from .models import Listing
 # Create your views here.
 
 
 def index(request):
-    listings= Listing.objects.all()
-    context={
-        'listings':listings
+    listings = Listing.objects.all()
+    # creat paginator
+    paginator= Paginator(listings,3)
+    page=request.GET.get('page')
+    paged_listings=paginator.get_page(page)
+    context = {
+        'listings': paged_listings
     }
-    return render(request, 'listings/listings.html',context)
+    return render(request, 'listings/listings.html', context)
 
 
-def listing(request,listing_id):
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 
