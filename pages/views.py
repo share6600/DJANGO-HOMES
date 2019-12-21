85dde19a1ca7138576a7bc6518025a641feaeee6
sharif <sharifmostafa1122@gmail.com>
2019-12-21 22:19:40 +0300

    refrence realtors model inside pages  views.py to for about page

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import Listing
+from realtors.models import Realtor
 def index(request):
     listings=Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
     context={
@@ -9,5 +10,12 @@ def index(request):
     return render(request,'pages/index.html',context)
 
 def about(request):
-    return render(request,'pages/about.html')
+    #all realtors
+    realtors=Realtor.objects.order_by('-hire_date')
+    mvp_realtors=Realtor.objects.order_by('-hire_date').filter(is_mvp=True)
+    context={
+        'realtors':realtors,
+        'mvp_realtors':mvp_realtors
+    }
+    return render(request,'pages/about.html',context)
 
