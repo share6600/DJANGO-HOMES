c9c930f77ff3f4659216ea0425a7a2010d2d1143
sharif <sharifmostafa1122@gmail.com>
2019-12-22 03:03:41 +0300

    send data on choice.py by search method on views to search.html

--- listings/views.py
+++ listings/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
 from .models import Listing
-from django.shortcuts import get_object_or_404# Create your views here.
+from django.shortcuts import get_object_or_404  # Create your views here.
+from .choices import bedroom_choices, price_choices, state_choices
 
 
 def index(request):
@@ -25,4 +26,9 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    context = {
+        'price_choices': price_choices,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choicesgit 
+    }
+    return render(request, 'listings/search.html', context)
