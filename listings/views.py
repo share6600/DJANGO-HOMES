e509608d57a21f6cb41d017c58f8d755931b37c8
sharif <sharifmostafa1122@gmail.com>
2019-12-27 07:06:11 +0300

    start creat query search

--- listings/views.py
+++ listings/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
 from .models import Listing
-from django.shortcuts import get_object_or_404  # Create your views here.
+from django.shortcuts import get_object_or_404 
 from .choices import bedroom_choices, price_choices, state_choices
 
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
     # creat paginator
-    paginator = Paginator(listings, 6)
+    paginator = Paginator(listings, 3)
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
     context = {
@@ -26,9 +26,21 @@ def listing(request, listing_id):
 
 
 def search(request):
-    context = {
-        'price_choices': price_choices,
-        'state_choices': state_choices,
-        'bedroom_choices': bedroom_choicesgit 
+    queryset_list= Listing.objects.order_by('-list_date')
+    #keywords
+    if 'keywords' in request.GET:
+      keywords=request.GET['keywords']
+      if keywords:
+          queryset_list=queryset_list.filter(description__icontains=keywords)
+    #city
+    if 'city' in request.GET:
+      city=request.GET['keywords']
+      if city:
+          queryset_list=queryset_list.filter(city__iexact=city)
+    context ={
+        'price_choices':price_choices,
+        'state_choices':state_choices,
+        'bedroom_choices':bedroom_choices,
+        'listings':queryset_list
     }
     return render(request, 'listings/search.html', context)
