a6cdf4fc75bef5384dd9c1df9172a8c2f7782235
sharif <sharifmostafa1122@gmail.com>
2019-12-29 19:08:34 +0300

    compleate search forums filtering

--- listings/views.py
+++ listings/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
 from .models import Listing
-from django.shortcuts import get_object_or_404 
-from .choices import bedroom_choices, price_choices, state_choices
+from django.shortcuts import get_object_or_404
+from listings.choices import bedroom_choices, price_choices, state_choices
 
 
 def index(request):
@@ -20,27 +20,28 @@ def index(request):
 def listing(request, listing_id):
     listing = get_object_or_404(Listing, pk=listing_id)
     context = {
-        'listing': listing
+        'listings': listing
     }
     return render(request, 'listings/listing.html', context)
 
 
 def search(request):
-    queryset_list= Listing.objects.order_by('-list_date')
-    #keywords
+    queryset_list = Listing.objects.order_by('-list_date')
+    # keywords
     if 'keywords' in request.GET:
-      keywords=request.GET['keywords']
-      if keywords:
-          queryset_list=queryset_list.filter(description__icontains=keywords)
-    #city
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(
+                description__icontains=keywords)
+    # city
     if 'city' in request.GET:
-      city=request.GET['keywords']
-      if city:
-          queryset_list=queryset_list.filter(city__iexact=city)
-    context ={
-        'price_choices':price_choices,
-        'state_choices':state_choices,
-        'bedroom_choices':bedroom_choices,
-        'listings':queryset_list
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+    context = {
+        'price_choices': price_choices,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'listings': queryset_list
     }
     return render(request, 'listings/search.html', context)
--- pages/views.py
+++ pages/views.py
@@ -7,9 +7,11 @@ def index(request):
     listings=Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
     context={
         'listings':listings,
-        'price_choices':price_choices,
         'state_choices':state_choices,
         'bedroom_choices':bedroom_choices,
+        'price_choices':price_choices,
+
+    
 
     }
     return render(request,'pages/index.html',context)
