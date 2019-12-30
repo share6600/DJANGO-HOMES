63e33e506ad930e689d64b980e4094030bbc79b9
sharif <sharifmostafa1122@gmail.com>
2019-12-30 04:20:35 +0300

    creat accounts app and intial html templates for them

---
+++ accounts/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect
+
+def register(request):
+    return render(request,'accounts/register.html')
+def login(request):
+    return render(request,'accounts/login.html')
+def logout(request):
+    return render('index')
+def dashboard(request):
+    return render(request,'accounts/dashbourd.html')
