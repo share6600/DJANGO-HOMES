2bfd8c5b8eafb09dae51711eecc1f964ac59d6a4
sharif <sharifmostafa1122@gmail.com>
2020-01-04 04:15:34 +0300

    previues change

--- listings/models.py
+++ listings/models.py
@@ -19,12 +19,12 @@ class Listing(models.Model):
   is_published=models.BooleanField(default=True)
   list_date=models.DateTimeField(default=datetime.now,blank=True)
   photo_main=models.ImageField(upload_to='photos/%y/%m/%d')
-  photo_1=models.ImageField(upload_to='photos/%y/%m/%d',blank=True)
-  photo_2=models.ImageField(upload_to='photos/%y/%m/%d',blank=True)
-  photo_3=models.ImageField(upload_to='photos/%y/%m/%d',blank=True)
-  photo_4=models.ImageField(upload_to='photos/%y/%m/%d',blank=True)
-  photo_5=models.ImageField(upload_to='photos/%y/%m/%d',blank=True)
-  photo_6=models.ImageField(upload_to='photos/%y/%m/%d',blank=True)
+  photo_1=models.ImageField(upload_to='%y/%m/%d',blank=True)
+  photo_2=models.ImageField(upload_to='%y/%m/%d',blank=True)
+  photo_3=models.ImageField(upload_to='%y/%m/%d',blank=True)
+  photo_4=models.ImageField(upload_to='%y/%m/%d',blank=True)
+  photo_5=models.ImageField(upload_to='%y/%m/%d',blank=True)
+  photo_6=models.ImageField(upload_to='%y/%m/%d',blank=True)
 
   def __str__(self):
     return self.title
